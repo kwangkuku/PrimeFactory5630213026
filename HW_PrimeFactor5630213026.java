e3c8b7c3be74c90399bc30aeaadbeb687ef6f790
KWANGkuku <kwangkuku@gmail.com>
2015-11-20 03:29:15 +0700

    CREATED HW_PrimeFactor5630213026.java

---
+++ HW_PrimeFactor5630213026.java
@@ -0,0 +1,25 @@
+package hw_primefactor5630213026;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Kwanrudee
+ */
+public class HW_PrimeFactor5630213026 {
+
+    int num;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter Number :  ");
+        int num = sc.nextInt();
+        
+        
+        for (int j=0; j <= num ; j++) {
+
+    }
+
+}
+}
